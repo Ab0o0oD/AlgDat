31e0381eb983bf174e3d7c0c7deaf72135d53641
Abod <abdulrzzak.kanjo@gmail.com>
2019-09-17 10:50:24 +0200

    løsning til oppgave 8 tekst  @Abo0o0oD

--- src/Oppgave8.java
+++ src/Oppgave8.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 /*Oppgave 8
     Lag metoden public static int [] indekssortering ( int [] a) . Den skal returnere en tabell
     med indekser til verdiene i tabellen a der a ikke skal endres. Se på. flg. eksempel:
@@ -25,4 +28,105 @@ for ( int i = 0; i < a. length ; i++) System. out .print(a[indeks[i]] + " " );
     forkommer to ganger, spiller det ingen rolle hvem av dem indeks[0] refererer til. Men da
     må indeks[1] referere til den andre av de to. Hvis a er tom, skal også returtabellen være tom.*/
 public class Oppgave8 {
+
+
+    //Her brukte jeg hjelpe metoder fra kompendiet:
+    public static void fratilKontroll(int tablengde, int fra, int til) {
+        if (fra < 0)                                  // fra er negativ
+            throw new ArrayIndexOutOfBoundsException
+                    ( "fra(" + fra + ") er negativ!" );
+
+        if (til > tablengde)                          // til er utenfor tabellen
+            throw new ArrayIndexOutOfBoundsException
+                    ( "til(" + til + ") > tablengde(" + tablengde + ")" );
+
+        if (fra > til)                                // fra er større enn til
+            throw new IllegalArgumentException
+                    ( "fra(" + fra + ") > til(" + til + ") - illegalt intervall!" );
+    }
+
+
+    public static int min(int[] a, int fra, int til) {
+
+        if (a == null) throw new NullPointerException
+                ( "parametertabellen a er null!" );
+
+        fratilKontroll( a.length, fra, til );
+
+        if (fra == til) throw new NoSuchElementException
+                ( "fra(" + fra + ") = til(" + til + ") - tomt tabellintervall!" );
+
+        int m = fra;             // indeks til minste verdi i a[fra:til>
+        int minsverdi = a[fra];  // minste verdi i a[fra:til>
+
+        for (int i = fra + 1; i < til; i++)
+            if (a[i] < minsverdi) {
+                m = i;               // indeks til minste verdi oppdateres
+                minsverdi = a[m];    // minste verdi oppdateres
+            }
+
+        return m;  // posisjonen til minst verdi i a[fra:til>
+    }
+
+    public static int maks(int[] a, int fra, int til) {
+
+        if (a == null) throw new NullPointerException
+                ( "parametertabellen a er null!" );
+
+        fratilKontroll( a.length, fra, til );
+
+        if (fra == til) throw new NoSuchElementException
+                ( "fra(" + fra + ") = til(" + til + ") - tomt tabellintervall!" );
+
+        int m = fra;             // indeks til største verdi i a[fra:til>
+        int maksverdi = a[fra];  // største verdi i a[fra:til>
+
+        for (int i = fra + 1; i < til; i++)
+            if (a[i] > maksverdi) {
+                m = i;               // indeks til største verdi oppdateres
+                maksverdi = a[m];    // største verdi oppdateres
+            }
+
+        return m;  // posisjonen til største verdi i a[fra:til>
+    }
+
+    public static int maks(int[] a)  // bruker hele tabellen
+    {
+        return maks( a, 0, a.length );  // kaller metoden over
+    }
+
+
+    public static int min(int[] a)  // bruker hele tabellen
+    {
+        return min( a, 0, a.length );  // kaller metoden over
+    }
+
+
+    // Kode bit for indeks sortering
+
+    public static int[] indekssortering(int[] a) {
+
+        if (a.length == 0) return a;
+
+        int[] temp = Arrays.copyOf( a, a.length );
+
+        int[] indekser = new int[a.length];
+
+        int maks = maks( temp );
+        int k = 0;
+
+        for (int i = a.length; i > 1; i--) {
+            int m = min( temp );
+            indekser[k] = m;
+            temp[m] = 2147483647;   //maks value for int
+            k++;
+        }
+        indekser[temp.length - 1] = maks;
+
+        for (int s : a) {
+            System.out.print( s + " " );
+        }
+        return indekser;
+    }
 }
+
