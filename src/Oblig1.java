9a8c3ab7fb733c6f2a7ecd852d18046aedb8e945
Abod <abdulrzzak.kanjo@gmail.com>
2019-09-17 11:44:32 +0200

    lagt inn oppgavet(1,2,3,5,6,7,8,10) inni oblig1.class og testet dem "feilfri" @Abo0o0oD

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -12,15 +12,102 @@ public class Oblig1 {
     }
 
     ///// Oppgave 1 //////////////////////////////////////
-    public static int maks(int[] a) { throw new NotImplementedException(); }
+    public static int maks(int[] a) {
+
+
+        if(a.length == 0){
+            throw new NoSuchElementException("Tabellen er tom");
+        }
+
+        int temp = 0;   //for mellomlagring av verdi
+
+        for(int i = 0; i<a.length-1; i++){
+            if(a[i]>a[i+1]){    //sammenligner et element, og elementet ved siden av
+                temp = a[i];    //mellomlagrer a[i]
+                a[i] = a[i+1];  //setter a[i] til å være neste element
+                a[i+1] = temp;  //setter a[i] til å være temp, veriden vi mellomlagret
+            }
+        }
+
+        int maks = a[a.length-1];   //maksverdi er da: verdien av siste element i arrayet
+        return maks;
+
+    }
+
+       /*
+    - Når blir det flest ombyttinger?
+      Når største verdi ligger først.
+
+    - Når blir det færrest?
+      Når største verdi ligger bakerst.
+
+    - Hvor mange blir det i gjennomsnitt?
+
+        Hvor mange operasjoner det blir i gjennomsnitt:
+        12+12n+9*log(n)-0,423
+
+        I gjennomsnitt antall a[i]>a[i+1] er sann,
+        altså hvor mange ombyttinger i gjennomsnitt: log(n)-0,423
+
+     */
+
 
     public static int ombyttinger(int[] a) {
-        throw new NotImplementedException();
+
+        if(a.length == 0){
+            throw new NoSuchElementException("Tabellen er tom");
+        }
+
+        int temp = 0;
+        int antallOmbyttinger = 0;
+
+        for(int i = 0; i<a.length-1; i++){
+            if(a[i]>a[i+1]){
+                temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                antallOmbyttinger++;
+            }
+        }
+
+        int maks = a[a.length-1];
+        return antallOmbyttinger;
     }
+
+
+    /*
+    - Indikasjon på hvor mange det blir i gjennomsnitt.
+      Er metoden maks bedre(eller dårligere) enn de maks-metodene
+      vi har sett på tidligere?
+
+        Mindre effektiv da den har flere antall operasjoner:
+        12+12n+9*log(n)-0,423
+
+     */
+
+
 
     ///// Oppgave 2 //////////////////////////////////////
     public static int antallUlikeSortert(int[] a) {
-        throw new NotImplementedException();
+        int antallUlike = 1;
+
+        if (a.length == 0){  //Sjekker om array er tomt, isåfall returneres 0
+            return 0;
+        }
+
+        for(int i = 0; i< a.length-1; i++){     //Sjekker om arrayet er sortert
+            if(a[i] > a[i+1]){
+                throw new IllegalStateException("Tabellen er ikke sortert stigende");
+            }
+        }
+
+        for(int i = 0; i<a.length-1; i++){  //Sjekker antall unike tall
+            if(a[i] < a[i+1]){
+                antallUlike++;
+            }
+        }
+
+        return antallUlike;
     }
 
 
@@ -80,28 +167,145 @@ public class Oblig1 {
 
     ///// Oppgave 5 //////////////////////////////////////
     public static void rotasjon(char[] a) {
-        throw new NotImplementedException();
+
+        // får hjelpevariabel n som er lengden til a.
+        int n = a.length;
+
+        // Her sjekker vi om lengden til a  er stor nok til å kunne
+        // gjøre en rotasjon. Ingenting skjer om n er mindre enn 2.
+        if (n<2) return;
+
+        // char b er hjelpevariabel som kopierer den siste i a.
+        char b = a[n-1];
+
+        // Under forflytter alle bokstavene i a et steg til høyre
+        for (int i = n - 1; i >= 1; i--) {
+            a[i] = a[i-1];
+        }
+        // Første indeks i a blir her satt til b, som var siste indeks før
+        // rotasjonen.
+        a[0] = b;
     }
 
     ///// Oppgave 6 //////////////////////////////////////
     public static void rotasjon(char[] a, int k) {
-        throw new NotImplementedException();
+        int n = a.length;
+        if (n<2) {
+            return; }
+        k %= n;
+        char[] b = new char[Math.abs(k)];
+        int c = 0;
+
+        // mot høyre
+        if (k > 0) {
+            for (int i = n - k; i < n; i++) {
+                b[c++] = a[i];
+            }
+            for (int i = n - k - 1; i >= 0; i--) {
+                a[k + i] = a[i];
+                if (i < k) {
+                    a[i] = b[i];
+                }
+            }
+
+            // mot venstre
+
+        } else if (k<0) {
+            k = Math.abs(k);
+            // Fra i = 0, mens i er mindre enn antall rotasjoner,
+            // 1 øker for hver gang. Vi kopierer
+            for(int i = 0; i<k; i++){
+                b[i] = a[i];
+            }
+
+            for(int i = 0; i <= n-k-1; i++){
+                a[i] = a[k+i];
+            }
+
+            for(int i = 0; i < b.length; i++){
+                a[n-i-1] = b[k - i - 1];
+            }
+
+        }
     }
 
     ///// Oppgave 7 //////////////////////////////////////
     /// 7a)
     public static String flett(String s, String t) {
-        throw new NotImplementedException();
+
+        int k = Math.min( s.length(), t.length() );
+
+
+        StringBuilder sp = new StringBuilder();
+
+        for (int i = 0; i < k; i++) {
+
+            sp.append( s.charAt( i ) ).append( t.charAt( i ) );
+        }
+
+        sp.append( s.substring( k ) ).append( t.substring( k ) );
+
+        return sp.toString();
     }
 
     /// 7b)
     public static String flett(String... s) {
-        throw new NotImplementedException();
+
+
+
+        int storstelength = 0;
+
+        for (int i = 0; i < s.length; i++) { //finner største string length i s Arrayet
+
+            if (s[i].length() > storstelength) {
+
+                storstelength = s[i].length();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= storstelength; i++) {
+
+            for (int j = 0; j <= s.length - 1; j++) {
+
+                if (i <= s[j].length()) {
+
+                    sb.append( s[j].charAt( i - 1 ) );
+                }
+            }
+        }
+        return sb.toString();
+
     }
 
     ///// Oppgave 8 //////////////////////////////////////
     public static int[] indekssortering(int[] a) {
-        throw new NotImplementedException();
+
+        if (a.length == 0) return a;
+
+        int[] temp = Arrays.copyOf( a, a.length );
+
+        int[] indekser = new int[a.length];
+
+        int maks = maksverdi( temp );
+        int k = 0;
+
+        for (int i = a.length; i > 1; i--) {
+            int m = min( temp );
+            indekser[k] = m;
+            temp[m] = 2147483647;   //maks value for int
+            k++;
+        }
+        indekser[temp.length - 1] = maks;
+
+        for (int s : a) {
+            System.out.print( s + " " );
+        }
+        return indekser;
+
+
+
     }
 
 
@@ -230,6 +434,84 @@ public class Oblig1 {
         if(i < right){
             quickSort(values, i, right);
         }
+    }
+
+
+
+
+//---------------------------------------------------------------------------------------//
+
+
+    //Her brukte jeg hjelpe metoder fra kompendiet:
+
+    public static void fratilKontroll(int tablengde, int fra, int til) {
+        if (fra < 0)                                  // fra er negativ
+            throw new ArrayIndexOutOfBoundsException
+                    ( "fra(" + fra + ") er negativ!" );
+
+        if (til > tablengde)                          // til er utenfor tabellen
+            throw new ArrayIndexOutOfBoundsException
+                    ( "til(" + til + ") > tablengde(" + tablengde + ")" );
+
+        if (fra > til)                                // fra er større enn til
+            throw new IllegalArgumentException
+                    ( "fra(" + fra + ") > til(" + til + ") - illegalt intervall!" );
+    }
+
+
+    public static int min(int[] a, int fra, int til) {
+
+        if (a == null) throw new NullPointerException
+                ( "parametertabellen a er null!" );
+
+        fratilKontroll( a.length, fra, til );
+
+        if (fra == til) throw new NoSuchElementException
+                ( "fra(" + fra + ") = til(" + til + ") - tomt tabellintervall!" );
+
+        int m = fra;             // indeks til minste verdi i a[fra:til>
+        int minsverdi = a[fra];  // minste verdi i a[fra:til>
+
+        for (int i = fra + 1; i < til; i++)
+            if (a[i] < minsverdi) {
+                m = i;               // indeks til minste verdi oppdateres
+                minsverdi = a[m];    // minste verdi oppdateres
+            }
+
+        return m;  // posisjonen til minst verdi i a[fra:til>
+    }
+
+    public static int maksverdi(int[] a, int fra, int til) {
+
+        if (a == null) throw new NullPointerException
+                ( "parametertabellen a er null!" );
+
+        fratilKontroll( a.length, fra, til );
+
+        if (fra == til) throw new NoSuchElementException
+                ( "fra(" + fra + ") = til(" + til + ") - tomt tabellintervall!" );
+
+        int m = fra;             // indeks til største verdi i a[fra:til>
+        int maksverdi = a[fra];  // største verdi i a[fra:til>
+
+        for (int i = fra + 1; i < til; i++)
+            if (a[i] > maksverdi) {
+                m = i;               // indeks til største verdi oppdateres
+                maksverdi = a[m];    // største verdi oppdateres
+            }
+
+        return m;  // posisjonen til største verdi i a[fra:til>
+    }
+
+    public static int maksverdi(int[] a)  // bruker hele tabellen
+    {
+        return maksverdi( a, 0, a.length );  // kaller metoden over
+    }
+
+
+    public static int min(int[] a)  // bruker hele tabellen
+    {
+        return min( a, 0, a.length );  // kaller metoden over
     }
 
 }
