0362b7e6a8d29033ab749ec91831ef5aa995be39
livel <livel@localhost>
2019-11-02 00:04:46 +0100

    Oppgave 3 nesteInorden og toString, og oppgave 10 remove

--- src/Oblig3/ObligSBinTre.java
+++ src/Oblig3/ObligSBinTre.java
@@ -101,16 +101,94 @@ public class ObligSBinTre<T> implements Beholder<T>
     throw new UnsupportedOperationException("Ikke kodet ennå!");
   }
   
+    
   private static <T> Node<T> nesteInorden(Node<T> p)
   {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+      Node<T> neste = null;
+      Node<T> ane = p.forelder;
+      
+      if (p.høyre != null)
+      {
+          neste = finnLavesteVenstre(p.høyre);
+      }
+      else if (ane != null && p == ane.venstre)
+      {
+          neste = ane;
+      }    
+      else if (p.høyre == null)
+      {
+          while (ane != null && p == ane.høyre)
+              {
+                p = ane;
+                ane = ane.forelder;
+              }
+          neste = ane;  
+      }   
+      else if (ane.høyre != null) 
+      {
+          neste = finnLavesteVenstre(ane.høyre);
+      }
+
+      else if (ane.høyre == null)
+      {
+          while (ane != null && p == ane.høyre)
+              {
+                p = ane;
+                ane = ane.forelder;
+              }
+          neste = ane;           
+      }
+      return neste;
+  
   }
+
+  
+  public static <T> Node<T> finnLavesteVenstre(Node<T> p)
+  { 
+      while (p != null)
+      {
+          if (p.venstre != null)
+         { 
+             p = p.venstre;
+        }
+        else return p;
+      }      
+      return p;
+  }
+  
   
   @Override
   public String toString()
-  {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
-  }
+  {      
+    StringBuilder sb = new StringBuilder();
+    
+    if (!tom())
+    {
+        Node<T> forsteNode, nesteNode = null;
+        forsteNode = finnLavesteVenstre(rot);
+        
+        sb.append("[");
+        sb.append(forsteNode.verdi);
+        
+        if (antall > 1)
+        {
+            nesteNode = nesteInorden(forsteNode);
+        }
+        while (nesteNode != null)
+        {
+            sb.append(',').append(' ').append(nesteNode.toString());
+            nesteNode = nesteInorden(nesteNode);
+        }
+        
+        sb.append("]");
+    }
+    
+    else sb.append("[]");
+    
+    return sb.toString();
+
+    }
+  
   
   public String omvendtString()
   {
@@ -171,12 +249,127 @@ public class ObligSBinTre<T> implements Beholder<T>
       throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
     
+    
     @Override
-    public void remove()
+        public void remove()
     {
-      throw new UnsupportedOperationException("Ikke kodet ennå!");
+      if (iteratorendringer != endringer) 
+      {  
+        throw new ConcurrentModificationException();
+      }
+      
+      if (q == null)
+      {
+        throw new IllegalStateException();
+      }
+      
+      if (bladNode(q))
+      {
+        if (q.forelder != null)
+        {
+            if (venstre(q)) q.forelder.venstre = null;
+            else if (hoeyre(q)) q.forelder.høyre = null;
+        }
+        
+        settForrigeBladnode();
+        
+        removeOK = false;
+        endringer++;
+        antall--;
+        iteratorendringer++;   
+      }
+      else 
+      {
+        throw new IllegalStateException();
+      }
+    }
+        
+            
+    public boolean hoeyre(Node<T> barn)
+    {
+        Node<T> ane = barn.forelder; 
+        return (barn == ane.høyre);
+    }
+    
+    public boolean venstre(Node<T> barn)
+    {
+        Node<T> ane = barn.forelder; 
+        return (barn == ane.venstre);
     }
 
+    public boolean bladNode(Node<T> node)
+    {
+        return (node.venstre == null && node.høyre == null);
+    }
+    
+    public void settForrigeBladnode() 
+    {
+      Node<T> passendeAne = finnPassendeAne(q, "hoeyre");
+     
+      if (passendeAne != null) 
+      {
+        finnLaveste(passendeAne.venstre);
+      }
+      
+      if (passendeAne == null) 
+      {
+          q = null;
+      }
+    }
+        
+    public void finnLaveste(Node<T> forrige) 
+    {
+        if (forrige.venstre == null && forrige.høyre == null)
+        {
+            q = forrige;
+            return;
+        }
+        else if (forrige.høyre != null)
+        {           
+            finnLaveste(forrige.høyre);
+        }  
+        else if (forrige.høyre == null)
+        {  
+            finnLaveste(forrige.venstre);
+        }
+    }
+    
   } // BladnodeIterator
+  
+  
+      public Node<T> finnPassendeAne(Node<T> foerste, String retning)
+    {
+        Node<T> ane = foerste.forelder;
+        
+        boolean t = passende(ane, foerste, retning);
+        
+        while (!t && ane != null)
+        {
+            foerste = ane;
+            ane = ane.forelder;
+            t = passende(ane, foerste, retning);
+        }
+        
+        if (passende(ane, foerste, retning)) 
+        {
+            return ane;
+        }
+        
+        else return null;
+    }
+    
+    public boolean passende(Node<T> ane, Node<T> barn, String retning)
+    {
+        if (ane == null) return false;
+        else if (retning == "venstre")
+        {
+            return (barn == ane.venstre && ane.høyre != null);
+        }
+        else if (retning == "hoeyre")
+        {
+            return (barn == ane.høyre && ane.venstre != null);
+        }
+        return false;
+    }
 
 } // ObligSBinTre
